846f4cb031f8f095d01b42fe2a475dcbafe3a798
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-12-15 16:31:20 -0800

    test: more failure tests.

---
+++ test/throw-test.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+/* eslint prefer-arrow-callback: "off" */
+
+'use strict';
+
+describe('Suite', () => {
+  throw new Error('Failure.');
+});
---
+++ test/uncaught-test.js
@@ -0,0 +1,25 @@
+/* eslint-env mocha */
+/* eslint prefer-arrow-callback: "off" */
+
+'use strict';
+
+describe('Exceptions', () => {
+  it('should fail (setImmediate)', () => {
+    setImmediate(() => {
+      throw new Error('foobar 1');
+    });
+  });
+
+  it('should not fail (setTimeout)', () => {
+    setTimeout(() => {
+      throw new Error('foobar 2');
+    }, 1);
+  });
+
+  it('should fail (setTimeout async)', (cb) => {
+    setTimeout(() => {
+      throw new Error('foobar 3');
+    }, 10);
+    setTimeout(cb, 50);
+  });
+});
